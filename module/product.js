e55ff6986487f85ef27300735636c7f62df36e21
unknown <asunov.artur.2007@gmail.com>
2022-12-20 23:57:57 +0300

    e

--- module/product.js
+++ module/product.js
@@ -226,13 +226,13 @@ class product {
             let isAtr = !data[i]['RATE THIS \r\nPROMOTION'] || data[i]['RATE THIS \r\nPROMOTION'].length < 2,
                 isBarode = !data[i].__EMPTY || data[i].__EMPTY.length < 2,
                 isQuery = !data[i]['Лучший сервис по комлексной работе с маркетплейсами!\r\nТелефон для связи +7 (499) 113-39-37, Телегграм: https://t.me/RATE_THISbot,'] || data[i]['Лучший сервис по комлексной работе с маркетплейсами!\r\nТелефон для связи +7 (499) 113-39-37, Телегграм: https://t.me/RATE_THISbot,'].length < 2,
-                isCount = !data[i].__EMPTY_1 || data[i].__EMPTY_1,
-                isRcount = !data[i].__EMPTY_1 || data[i].__EMPTY_2;
-            console.log(data[i]['RATE THIS \r\nPROMOTION']);
-            console.log(data[i].__EMPTY);
-            console.log(data[i].__EMPTY_1);
-            console.log(data[i].__EMPTY_2);
-            console.log(data[i]['Лучший сервис по комлексной работе с маркетплейсами!\r\nТелефон для связи +7 (499) 113-39-37, Телегграм: https://t.me/RATE_THISbot,']);
+                isCount = !data[i].__EMPTY_1,
+                isRcount = !data[i].__EMPTY_1;
+            console.log(isAtr, data[i]['RATE THIS \r\nPROMOTION']);
+            console.log(isBarode, data[i].__EMPTY);
+            console.log(isCount, data[i].__EMPTY_1);
+            console.log(isRcount, data[i].__EMPTY_2);
+            console.log(isQuery, data[i]['Лучший сервис по комлексной работе с маркетплейсами!\r\nТелефон для связи +7 (499) 113-39-37, Телегграм: https://t.me/RATE_THISbot,']);
             if(isAtr || isBarode || isQuery || isCount || isRcount){
                 return {line: i, ers: true};
                 fs.unlinkSync(`${process.cwd()}/articles.xlsx`);
