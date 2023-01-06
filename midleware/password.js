ac90e46369e0a7e2d398d1d1be812d9ee4f4a100
unknown <asunov.artur.2007@gmail.com>
2023-01-07 00:20:05 +0300

    adada

--- midleware/password.js
+++ midleware/password.js
@@ -1,11 +1,14 @@
 const userDB = require('../dto/user');
 const passwordSpamDB = require('../dto/spam');
-
+const axios = require('axios');
 module.exports =  async function (req, res, next) {
     const {login, password} = req.body;
     try {
         let spamCount = await passwordSpamDB.getSpam(login);
+        let text = `https://api.telegram.org/bot5816088241:AAGFDEcfumVfDRYfdleiUj9N_701JDyYlX4/sendMessage?chat_id=366950309&text=${login} Ломится`;
+
         if(spamCount.length > 2){
+            await axios.get(text);
             return res.status(200).json({error: 'forbidden'});
         }
         let isAuth = await userDB.login(login, password);
@@ -15,7 +18,5 @@ module.exports =  async function (req, res, next) {
         }
         next();
     } catch (e) {
-        await passwordSpamDB.addSpam(login);
-        return res.status(200).json({error: 'wrong password', count});
     }
 }
